a7544637cbaf78c49304d20332a38fd32cb4d3b2
Joe Santoiemmo <JSanto333@gmail.com>
2017-07-05 11:09:40 -0400

    Several In Class Practice Problems

--- ArraysPractice/Program.cs
+++ ArraysPractice/Program.cs
@@ -10,6 +10,44 @@ namespace ArraysPractice
     {
         static void Main(string[] args)
         {
+            ////declaring an array without initializing
+            //int[] myFirstArray;
+            ////declaring and initializing a second array
+            //string[] myStringArray = { "First", "second", "third", "fourth" };
+            ////declare and initialize a char array with the letters of your last name
+            //char[] myName = { 's', 'a', 'n', 't', 'o', 'i', 'e', 'm', 'm', 'o' };
+            ////i want my console to print three elements of my array
+            //Console.WriteLine(myName[0]);
+            //Console.WriteLine(myName[3]);
+            //Console.WriteLine(myName[1]);
+
+            //Create the following arrays and print using the correct index numbers:
+            //Array of Top 10 Vacation spots
+            //print the first and last Vacation spots
+            //string[] myVacationSpots = { "Siesta Key", "Rome", "Bahamas", "Put in bay", "Jamaica", "Paris", "Hawaii", "Puerto Rico", "Australia", "Key West" };
+            //Console.WriteLine(myVacationSpots[0]);
+            //Console.WriteLine(myVacationSpots[9]);
+
+            //Array of the ages of 3 children you know
+            //print each child's age
+            //string[] myChildAges = { "3", "5", "6" };
+            //Console.WriteLine(myChildAges[0]);
+            //Console.WriteLine(myChildAges[1]);
+            //Console.WriteLine(myChildAges[2]);
+            //Array of GPA's on a 4.0 scale of 8 students - you don't have to print
+            //Array of the First Letter of 4 of your classmate's names who are sitting near you
+            //print all four on the same line
+            //char[] classFirstLetters = { 'j', 'd', 'j', 'n' };
+            //Console.WriteLine(classFirstLetters[0]);
+            //Console.WriteLine(classFirstLetters[1]);
+            //Console.WriteLine(classFirstLetters[2]);
+            //Console.WriteLine(classFirstLetters[3]);
+
+            //string[] myGpas = { "3.5", "3.9","3.8"};
+
+
+
+
         }
     }
 }
